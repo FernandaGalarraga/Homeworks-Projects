0a32fb953f6ef7acfc7a450218b3980076e5693b
fgalarraga1999 <maferita_kiut10@live.com>
2019-01-17 22:33:10 -0500

    Lab23

---
+++ Projects/ThirdPartial/LAB23-MenusAndInteractions/src/lab23/menusandinteractions/LAB23MenusAndInteractions.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab23.menusandinteractions;
+
+/**
+ *
+ * @author Fernanda Galárraga
+ */
+public class LAB23MenusAndInteractions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
